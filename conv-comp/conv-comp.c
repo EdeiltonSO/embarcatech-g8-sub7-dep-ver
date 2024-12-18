2db67e109b55fe03695aa3b8035aa54058948efb
Kuroikenshiga <juniorsilvapalafoz@gmail.com>
2024-12-17 21:36:07 -0300

    feat: implementação do conversor de unidade de comprimento

--- conv-comp/conv-comp.c
+++ conv-comp/conv-comp.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 
+//Conversão de metros
+float metroParaCentimetro(float medidaMetro);
+float metroParaMilimetro(float medidaMetro);
+
+//Conversão de centimetros
+float centimetroParaMetro(float medidaCentimetro);
+float centimetroParaMilimetro(float medidaCentimetro);
+
+//Conversão de milimetros
+float milimetroParaMetro(float medidaMilimetro);
+float milimetroParaCentimetro(float medidaMilimetro);
+
 int main() {
 
-    printf("Conversor de comprimento");
+    printf("%.2f",milimetroParaCentimetro(100));
+
     return 0;
+}
+
+//Conversão de metros
+float metroParaCentimetro(float medidaMetro){
+    return medidaMetro*100;
+}
+float metroParaMilimetro(float medidaMetro){
+    return medidaMetro*1000;
+}
+
+//Conversão de centimetros
+float centimetroParaMetro(float medidaCentimetro){
+    return medidaCentimetro/100;
+}
+float centimetroParaMilimetro(float medidaCentimetro){
+    return medidaCentimetro*10;
+}
+
+//Conversão de milimetros
+float milimetroParaMetro(float medidaMilimetro){
+    return medidaMilimetro/1000;
+}
+float milimetroParaCentimetro(float medidaMilimetro){
+    return medidaMilimetro/10;
 }
