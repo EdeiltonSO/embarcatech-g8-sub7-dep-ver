8c5938f59c5189014b924aa2ab54305d0b2da2ff
Valnei Sousa <vscfm@protonmail.com>
2024-12-18 11:24:40 -0300

    Update: Conversão de medidas de velocidade estruturada e testada.

--- conv-vel/conv-vel.c
+++ conv-vel/conv-vel.c
@@ -2,105 +2,87 @@
 
 int main() {
     int opcaoini, opcaofim;
-    float vms, vmphs, vkmhs;
-    const mstomph = 2.2369; // Constante de Conversão de Metros por Segundo Para Milhas Por Hora
-    const kmhtoms = 3.6; // Constante de Conversão de Metros por Segundo para Quilômetros por Hora
-    const kmhtomph = 0.621371; // Constante de Quilômetros por Hora para Milhas por Hora
-    printf("Bem vindo ao Conversor de velocidade\n");
+    float vms = 0.0, vmphs = 0.0, vkmhs = 0.0, convfim = 0.0;
+    #define mstomph 2.2369; // Constante de Conversão de Metros por Segundo Para Milhas Por Hora
+    #define kmhtoms  3.6; // Constante de Conversão de Metros por Segundo para Quilômetros por Hora
+    #define kmhtomph 0.621371; // Constante de Quilômetros por Hora para Milhas por Hora
+    printf("Bem vindo ao Conversor de velocidade \n");
     printf("Escolha a primeira unidade para Conversão:\n");
-    printf("1 - Metros por Segundo/n 2 - Quilômetros por Hora\n 3 - Milhas por Hora/n");
-    scanf("%d", opcaoini);
-    printf("Escolha a segunda unidade para Conversão:\n");
-    printf("1 - Metros por Segundo/n 2 - Quilômetros por Hora\n 3 - Milhas por Hora/n");
-    scanf("%d", opcaofim);
+    printf("1 - Metros por Segundo \n 2 - Quilômetros por Hora \n 3 - Milhas por Hora \n");
+    scanf("%d", &opcaoini);
+    printf("Escolha a segunda unidade para Conversão: \n");
+    printf("1 - Metros por Segundo \n 2 - Quilômetros por Hora \n 3 - Milhas por Hora \n");
+    scanf("%d", &opcaofim);
     
-    if (opcaoini == 1)
+    if (opcaoini == 1) 
     {
-        // Caso a unidade seja Metros por Segundo
-        printf("Informe o Valor para Conversão em Metros por Segundo (Informe apenas os Números/n)");
-        scanf("%f", vms);    
+        printf("\nInforme o valor em Metros por Segundo: ");
+        scanf("%f", &vms);
+    } 
+    
+    else if (opcaoini == 2) 
+    {
+        printf("\nInforme o valor em Quilômetros por Hora: ");
+        scanf("%f", &vkmhs);
+    } 
+    
+    else if (opcaoini == 3) 
+    {
+        printf("\nInforme o valor em Milhas por Hora: ");
+        scanf("%f", &vmphs);
+    } 
+    
+    else {
+        printf("Opção inválida!\n");
+        return 1;
     }
     
-    if (opcaoini == 2)
-    {
-        // Caso a unidade seja Quilômetros por Hora
-        printf("Informe o Valor para Conversão em Quilômetros por Hora (Informe apenas os Números/n)");
-        scanf("%f", vkmhs);
-    }
-    
-    if (opcaoini == 3)
-    {
-        // Caso a unidade seja Milhas por Hora
-        printf("Informe o Valor para Conversão em Milhas por Hora (Informe apenas os Números/n)");
-        scanf("%f", vmphs);
-    }
+    // Conversões
 
-    
-    if (opcaofim == 1)
-    {
-        // Caso a unidade seja Metros por Segundo
-        printf("Informe o Valor para Conversão em Metros por Segundo (Informe apenas os Números/n)");
-        scanf("%f", vms);
-    }
-    
-    if (opcaofim == 2)
-    {
-        // Caso a unidade seja Quilômetros por Hora
-        printf("Informe o Valor para Conversão em Quilômetros por Hora (Informe apenas os Números/n)");
-        scanf("%f", vkmhs);
-    }
-    
-    if (opcaofim == 3)
-    {
-        // Caso a unidade seja Milhas por Hora
-        printf("Informe o Valor para Conversão em Milhas por Hora (Informe apenas os Números/n)");
-        scanf("%f", vmphs);
-    }
-
-    if (opcaoini == 1 && opcaofim == 1)
-    {
-
-    }
-    
     if (opcaoini == 1 && opcaofim == 2)
     {
-        
+        convfim = vms * kmhtoms;
+        printf("O valor convertido é: %.2f Quilômetros por Hora\n", convfim);
     }
     
-    if (opcaoini == 1 && opcaofim == 3)
+    else if (opcaoini == 1 && opcaofim == 3)
     {
-        
+        convfim = vms * mstomph;
+        printf("O valor convertido é: %.2f Milhas por Hora\n", convfim);
     }
     
-    if (opcaoini == 2 && opcaofim == 1)
+    else if (opcaoini == 2 && opcaofim == 1)
     {
-        
+        convfim = vkmhs / kmhtoms;
+         printf("O valor convertido é: %.2f Metros por Segundo\n", convfim);
     }
     
-    if (opcaoini == 2 && opcaofim == 2)
+    else if (opcaoini == 2 && opcaofim == 3)
     {
-        
-    }
-    
-    if (opcaoini == 2 && opcaofim == 3)
-    {
-        
+        convfim = vkmhs * kmhtomph;
+        printf("O valor convertido é: %.2f Milhas por Hora\n", convfim);
     }
 
-    if (opcaoini == 3 && opcaofim == 1)
+    else if (opcaoini == 3 && opcaofim == 1)
     {
-        
+        convfim = vmphs / mstomph ;
+        printf("O valor convertido é: %.2f Metros por Segundo\n", convfim);
     }
 
-    if (opcaoini == 3 && opcaofim == 2)
+    else if (opcaoini == 3 && opcaofim == 2)
     {
-        
+        convfim = vmphs / kmhtomph;
+        printf("O valor convertido é: %.2f Quilômetros por Hora\n", convfim);
     }
 
-    if (opcaoini == 3 && opcaofim == 3)
-    {
-        
+    else if (opcaoini == opcaofim) {
+        printf("A unidade inicial e final são iguais. O valor permanece o mesmo.\n");
+    } 
+    
+    else {
+        printf("Opção inválida!\n");
     }
+
     return 0;
 }
 
