3806bbf444e3ba0e6dab12a5369caf07b51d8de0
Astorolus-11 <alvesgustavao@gmail.com>
2024-12-19 18:12:36 -0300

    Adicionado biblioteca locale.h, estrutura de repetição e foi aumentado a precisão em 4 decimais

--- conv-tempo/conv-tempo.c
+++ conv-tempo/conv-tempo.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main(){
 
+    setlocale(LC_ALL,"portuguese");
     int opc;
     double segundos,minutos,horas;
-inicio:
+
+do{
     printf("Qual tipo de conversao de tempo deseja fazer?\n");
     printf("1 - Segundos para minutos.\n");
     printf("2 - Segundos para Horas.\n");
@@ -12,14 +15,16 @@ inicio:
     printf("4 - Minutos para horas.\n");
     printf("5 - Horas para segundos.\n");
     printf("6 - Horas para minutos.\n");
+    printf("0 - Sair.\n");
     scanf("%d",&opc);
 
-    switch (opc){
+    switch (opc){ //cases das opções
+
         case 1:{
             printf("Digite quantos segundos:\n");
             scanf("%lf",&segundos);
             minutos=segundos/60;
-            printf("%.2f segundos em minutos = %.0fmin", segundos,minutos);
+            printf("%.2f segundos em minutos = %.4fmin\n", segundos,minutos);
             break;
         }
         case 2:{
@@ -27,7 +32,7 @@ inicio:
             scanf("%lf",&segundos);
             horas=segundos/3600;
             minutos=segundos/60;
-            printf("%.2f segundos em horas = %.2fh", segundos,horas);
+            printf("%.2f segundos em horas = %.4fh\n", segundos,horas);
 
             break;
         }
@@ -35,14 +40,14 @@ inicio:
             printf("Digite quantos minutos:\n");
             scanf("%lf",&minutos);
             segundos=minutos*60;
-            printf("%.2f minutos em segundos = %.2fseg", minutos,segundos);
+            printf("%.2f minutos em segundos = %.4fseg\n", minutos,segundos);
             break;
         }
         case 4:{
             printf("Digite quantos minutos:\n");
             scanf("%lf",&minutos);
             horas=minutos/60;
-            printf("%.2f minutos em horas = %.2fh", minutos,horas);
+            printf("%.2f minutos em horas = %.4fh\n", minutos,horas);
 
             break;
         }
@@ -50,7 +55,7 @@ inicio:
             printf("Digite quantas horas:\n");
             scanf("%lf",&horas);
             segundos=horas*3600;
-            printf("%.2f horas em segundos = %.2fseg",horas,segundos);
+            printf("%.2f horas em segundos = %.4fseg\n",horas,segundos);
 
             break;
         }
@@ -58,16 +63,26 @@ inicio:
             printf("Digite quantas horas:\n");
             scanf("%lf",&horas);
             minutos=horas*60;
-            printf("%.2f horas em minutos = %.2fmin",horas,minutos);
+            printf("%.2f horas em minutos = %.4fmin\n",horas,minutos);
 
             break;
         }
-
-  
-         default:
-        printf("Opcaoo invalida! Digite novamente.\n");
-        goto inicio;
-        break;
+        case 0:{
+            printf("Fim da conversão\n");
+            break;
         }
 
+        default:{
+          printf("Opção inválida! Digite novamente.\n");
+        break;
+        }
+        
+        }
+}while(opc!=0);
+
+
+
+
 }
+   
+
